bb0e575977a71921491b8e0aaa4c628d76fe3794
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-10-04 00:23:36 +0300

    Remove locationSlice and store files

--- src/features/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import locationReducer from './location/locationSlice'
-
-export default configureStore({
-  reducer: {
-    location: locationReducer,
-  }
-})
