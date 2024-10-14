79297b087970ccc415f96415a151f0b8ca85cfc2
Berat Murathan Bayram <brtbyram.61@gmail.com>
2024-10-14 17:05:49 +0300

    Refactor vite.config.js to use @vitejs/plugin-react instead of vite-plugin-commonjs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import commonjs from 'vite-plugin-commonjs';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [commonjs()],
+  plugins: [react()],
 })
